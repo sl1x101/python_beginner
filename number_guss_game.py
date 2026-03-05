b5d215de497edb7e89ea9df6b13a655c28c1c11a
sl1x101 <rew2540@gmail.com>
2026-03-05 11:16:27 +0900

    add number guess

---
+++ number_guss_game.py
@@ -0,0 +1,17 @@
+#number mean is random a number (1-20)
+import random as rd
+
+random_to_number = rd.randint(1,20)
+
+while True:
+    try:
+        guess = int(input("input put your number (1-20): "))     
+        if guess < random_to_number:
+            print("to low")
+        elif guess> random_to_number:
+            print("to height")
+        else:
+            print("Congratulation! you guess the number! ")
+            break
+    except ValueError:
+        print("input number!")
