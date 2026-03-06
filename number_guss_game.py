cce328d73a32a980134e115689bf7e71c7a40949
sl1x101 <rew2540@gmail.com>
2026-03-06 18:43:51 +0900

    commit 3

--- number_guss_game.py
+++ number_guss_game.py
@@ -1,8 +1,6 @@
-#number mean is random a number (1-20)
+#number random  (1-20)
 import random as rd
-
 random_to_number = rd.randint(1,20)
-
 while True:
     try:
         guess = int(input("input put your number (1-20): "))     
